8db40c45ce54a466c15a8cc7ba5f253764572f7f
Name <comteprovisoire@gmail.com>
2022-03-18 16:49:47 +0100

    back-end init

---
+++ back/src/main/java/com/csid/justread/JustReadApplication.java
@@ -0,0 +1,13 @@
+package com.csid.justread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustReadApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/csid/justread/JustReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csid.justread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

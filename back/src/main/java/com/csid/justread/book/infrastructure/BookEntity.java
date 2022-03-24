983090bf6a27f83883fdbdf6a395c342ac1172df
Roms77 <romainmignac@gmail.com>
2022-03-24 12:09:24 +0100

    Changement bdd

--- back/src/main/java/com/csid/justread/book/infrastructure/BookEntity.java
+++ back/src/main/java/com/csid/justread/book/infrastructure/BookEntity.java
@@ -1,4 +1,13 @@
 package com.csid.justread.book.infrastructure;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name="book")
+@Table(name="jr_book")
 public class BookEntity {
+
+
+
+
 }

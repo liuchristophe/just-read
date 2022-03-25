c8b8f23bec167001ba318f097bfdfd0e1624cd83
liuchristophe <liuchristophe3@gmail.com>
2022-03-25 16:35:23 +0100

    [ADD/RM/MV] déplacer la page angular ailleur

--- front/src/app/shared/components/components.module.ts
+++ front/src/app/shared/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularComponent } from './angular/angular.component';
 
 
 
 @NgModule({
   declarations: [
+    AngularComponent
   ],
   imports: [
     CommonModule
   ]
 })
-export class SharedModule { }
+export class ComponentsModule { }

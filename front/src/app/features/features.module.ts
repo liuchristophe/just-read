535f8d0b1a222b7c5618578979ea42dec2363f07
Rémi Marques de Jésus <marquesremi94120@gmail.com>
2022-03-31 12:50:27 +0000

    git

--- front/src/app/features/features.module.ts
+++ front/src/app/features/features.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormAjoutLivreComponent } from './form-ajout-livre/form-ajout-livre.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormAjoutLivreComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ front/src/app/features/form-ajout-livre/form-ajout-livre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-ajout-livre',
+  templateUrl: './form-ajout-livre.component.html',
+  styleUrls: ['./form-ajout-livre.component.scss']
+})
+export class FormAjoutLivreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

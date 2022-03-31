6084a5bbd819431c6c9583c3eff9041bf6918d87
d.abouadaou <danyabouadaou@gmail.com>
2022-03-31 15:54:00 +0200

    fixes

--- back/src/main/java/com/csid/justread/JustReadApplication.java
+++ back/src/main/java/com/csid/justread/JustReadApplication.java
@@ -2,11 +2,6 @@ package com.csid.justread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
 
 import java.io.File;
 

fb7074f63d892789d9913fcfba9e00235ad92a98
Romain <romain.mignac@etu.u-pec.fr>
2022-04-01 16:57:43 +0200

    Ajout author à book

--- back/src/main/java/com/csid/justread/book/infrastructure/dao/AuthorDao.java
+++ back/src/main/java/com/csid/justread/book/infrastructure/dao/AuthorDao.java
@@ -4,6 +4,9 @@ import com.csid.justread.book.infrastructure.entity.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorDao extends JpaRepository<AuthorEntity, Long> {
+    Optional<AuthorEntity> findByLastnameAndFirstnameAndPseudo(String lastname, String firstname, String pseudo);
 }

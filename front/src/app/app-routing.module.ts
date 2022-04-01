44f83ae599855aff596d9181fc0068e0c9f023f7
Rémi <marquesremi94120@gmail.com>
2022-04-01 10:48:15 +0200

    ajout lien ajoutLivre dans le nav + début routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormAjoutLivreComponent } from './features/form-ajout-livre/form-ajout-livre.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '**', component: PageNotFoundComponent},
+  {path: 'ajoutLivre', component: FormAjoutLivreComponent}
 ];
 
 @NgModule({

2855294d8ba720a6c4d740cc49c23a6256515696
Dany <danyabouadaou@gmail.com>
2022-04-08 15:43:43 +0200

    Update : Mappers

--- back/src/main/java/com/csid/justread/book/CategoryMapper.java
+++ back/src/main/java/com/csid/justread/book/CategoryMapper.java
@@ -1,42 +1,20 @@
 package com.csid.justread.book;
 
-import com.csid.justread.book.domain.model.Book;
-import com.csid.justread.book.domain.model.Category;
-import com.csid.justread.book.exposition.dto.BookDto;
 import com.csid.justread.book.exposition.dto.CategoryDto;
-import com.csid.justread.book.infrastructure.entity.BookEntity;
 import com.csid.justread.book.infrastructure.entity.CategoryEntity;
 
 public class CategoryMapper {
 
-    public static CategoryDto categoryToDto(Category category) {
+    public CategoryDto entityToDto ( CategoryEntity c ) {
         CategoryDto result = new CategoryDto();
-        result.setName(category.getName());
-        result.setId(category.getId());
+        result.setName(c.getName());
+        result.setId(c.getId());
         return result;
     }
-
-    public static Category dtoToCategory ( CategoryDto categoryDto )
-    {
-        Category result = new Category();
-        result.setName(categoryDto.getName());
-        result.setId(categoryDto.getId());
-        return result;
-    }
-
-    public static Category entityToCategory(CategoryEntity categoryEntity) {
-
-        Category result = new Category();
-        result.setName(categoryEntity.getName());
-        result.setId(categoryEntity.getId());
-        return result;
-    }
-
-    public static CategoryEntity categoryToEntity(Category category) {
-
+    public CategoryEntity dtoToEntity ( CategoryDto c ) {
         CategoryEntity result = new CategoryEntity();
-        result.setName(category.getName());
-        result.setId(category.getId());
+        result.setName(c.getName());
+        result.setId(c.getId());
         return result;
     }
 }
--- back/src/main/java/com/csid/justread/book/PublisherMapper.java
+++ back/src/main/java/com/csid/justread/book/PublisherMapper.java
@@ -1,40 +1,21 @@
 package com.csid.justread.book;
 
-import com.csid.justread.book.domain.model.Publisher;
 import com.csid.justread.book.exposition.dto.PublisherDto;
 import com.csid.justread.book.infrastructure.entity.PublisherEntity;
 
 public class PublisherMapper {
 
-    public static Publisher dtoToPublisher(PublisherDto p) {
-
-        Publisher result = new Publisher ();
-        result.setId( p.getId() );
-        result.setName( p.getName() );
-        return result;
-    }
-
-    public static PublisherDto publisherToDto(Publisher p) {
-
+    public static PublisherDto entityToDto ( PublisherEntity p ) {
         PublisherDto result = new PublisherDto ();
         result.setId( p.getId() );
         result.setName( p.getName() );
         return result;
     }
-
-    public static Publisher entityToPublisher(PublisherEntity p) {
-
-        Publisher result = new Publisher ();
-        result.setId( p.getId() );
-        result.setName( p.getName() );
-        return result;
-    }
-
-    public static PublisherEntity publisherToEntity(Publisher p) {
-
+    public static PublisherEntity entityToDto ( PublisherDto p ) {
         PublisherEntity result = new PublisherEntity ();
         result.setId( p.getId() );
         result.setName( p.getName() );
         return result;
     }
+
 }

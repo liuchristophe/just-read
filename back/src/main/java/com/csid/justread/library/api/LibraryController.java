dabc1ce0507b504f53e9db3763b51a4689033eb8
Dany <danyabouadaou@gmail.com>
2022-04-21 17:30:58 +0200

    Stock

--- back/src/main/java/com/csid/justread/library/api/LibraryController.java
+++ back/src/main/java/com/csid/justread/library/api/LibraryController.java
@@ -2,17 +2,14 @@ package com.csid.justread.library.api;
 
 import com.csid.justread.Converter;
 import com.csid.justread.library.api.dto.StockDto;
-import com.csid.justread.Converter;
 import com.csid.justread.library.api.dto.LibraryDto;
 import com.csid.justread.library.service.LibraryService;
+import com.csid.justread.library.service.model.Stock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 import com.csid.justread.library.service.model.Library;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController()
 @RequestMapping("/api/library")
@@ -22,6 +19,30 @@ public class LibraryController {
     public LibraryController(LibraryService libraryService) {
         this.libraryService = libraryService;
     }
+
+    //region * Library Management *
+
+    @PostMapping()
+    public LibraryDto createLibrary(@RequestBody LibraryDto libraryDto){
+        return new Converter().map(
+                libraryService.create(
+                        new Converter().map(libraryDto, Library.class)
+                ), LibraryDto.class);
+    }
+    @GetMapping()
+    public List<LibraryDto> getLibraries(){
+        List<Library> lib = libraryService.getLibraries();
+        return new Converter().mapAsList(lib , LibraryDto.class);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<StockDto> getLibraryById( @PathVariable("uuid") UUID id ) {
+        return ResponseEntity.ok(
+                new Converter().map( this.libraryService.getLibraryById( id ).get(), StockDto.class)
+        );
+    }
+
+    //endregion
 
     //region * Stock Management *
 
@@ -37,26 +58,13 @@ public class LibraryController {
         );
     }
 
-    @PatchMapping()
-    public ResponseEntity updateStock() {
-        return null; /** TODO : mises à jour de stock **/
+    /*
+    @PostMapping("/stock/{uuid}")
+    public ResponseEntity<StockDto> createStock(@PathVariable("uuid") UUID idLibrary, @RequestBody StockDto stock) {
+        this.libraryService.createStock(idLibrary, new Converter().map( stock, Stock.class ));
+        return null;
     }
+    */
 
     //endregion
-
-
-
-    @PostMapping()
-    public LibraryDto create(@RequestBody LibraryDto libraryDto){
-        return new Converter().map(
-                libraryService.create(
-                        new Converter().map(libraryDto, Library.class)
-                ), LibraryDto.class);
-    }
-
-    @GetMapping()
-    public List<LibraryDto> getLibraries(){
-        List<Library> lib = libraryService.getLibraries();
-        return new Converter().mapAsList(lib , LibraryDto.class);
-    }
 }
--- back/src/main/java/com/csid/justread/library/service/LibraryService.java
+++ back/src/main/java/com/csid/justread/library/service/LibraryService.java
@@ -3,14 +3,15 @@ package com.csid.justread.library.service;
 import com.csid.justread.Converter;
 import com.csid.justread.library.infrastructure.dao.LibraryDao;
 import com.csid.justread.library.infrastructure.dao.StockDao;
+import com.csid.justread.library.infrastructure.entity.StockEntity;
 import com.csid.justread.library.service.model.Stock;
 import com.csid.justread.library.infrastructure.entity.LibraryEntity;
 import com.csid.justread.library.service.model.Library;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.List;
 
 @Service
 @Transactional
@@ -23,22 +24,56 @@ public class LibraryService {
         this.stockDao   = stockDao;
     }
 
-    public List<Stock> getStock() {
-        return new Converter().mapAsList( this.stockDao.findAll(), Stock.class );
-    }
-
-    public Stock getStockById( UUID id ) {
-        return new Converter().map( this.stockDao.findById( id ), Stock.class );
-    }
+    //region * Library Management *
 
     public Library create(Library library) {
         return new Converter().map(
                 libraryDao.save(
-                    new Converter().map(library, LibraryEntity.class)
+                        new Converter().map(library, LibraryEntity.class)
                 ), Library.class);
     }
 
     public List<Library> getLibraries(){
         return new Converter().mapAsList(libraryDao.findAll() , Library.class);
     }
+
+    public Optional<Library> getLibraryById( UUID id ) {
+        Library l = new Converter().map( this.libraryDao.findById( id ), Library.class );
+        return Optional.of(l);
+    }
+
+    //endregion
+
+    //region * Stock Management *
+
+    public List<Stock> getStock() {
+        return new Converter().mapAsList( this.stockDao.findAll(), Stock.class );
+    }
+
+    public Optional<Stock> getStockById( UUID id ) {
+        Stock s  = new Converter().map( this.stockDao.findById( id ), Stock.class );
+        return Optional.of(s);
+    }
+
+    public Stock createStock( UUID idLibrary, Stock stock ) {
+
+        Optional<Stock> optStock = ( stock.getId() != null ) ? this.getStockById( stock.getId() ) : Optional.empty();
+        Optional<Library> optLibrary = this.getLibraryById( idLibrary );
+
+        Stock   s = null;
+
+        if ( optLibrary.isPresent() ) {
+
+            if (optStock.isPresent()) {
+                s = optStock.get();
+                s.setLibrary( optLibrary.get() );
+                s = new Converter().map( this.stockDao.save( new Converter().map( s, StockEntity.class) ), Stock.class );
+            }
+        }
+
+        return  s;
+    }
+
+    //endregion
+
 }

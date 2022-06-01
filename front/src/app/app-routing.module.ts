7605f1c661197003add2c02004ad3df1e0aca6c0
Rémi <marquesremi94120@gmail.com>
2022-06-01 23:13:55 +0200

    mutliple changes (début overlay + upgrade visu)
    Co-authored-by: liuchristophe <liuchristophe@users.noreply.github.com>

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'ajoutLivre', component: FormAjoutLivreComponent},
   {path : 'bookList',component: BookListComponent}, 
-  {path : 'book/:id', component: BookDetailsComponent},
+  // {path : 'book/:id', component: BookDetailsComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 

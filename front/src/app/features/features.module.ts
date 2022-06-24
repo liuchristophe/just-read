24c4fc4794703584f81dcbe02130444e5b8b0ffc
liuchristophe <liuchristophe3@gmail.com>
2022-06-24 17:32:40 +0200

    Message dialog après ajouter

--- front/src/app/features/features.module.ts
+++ front/src/app/features/features.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { BookModule } from './book/book.module';
 import { LibraryPresentationComponent } from './library-presentation/library-presentation.component';
-import { FormAjoutLivreComponent } from './dashboard/form-ajout-livre/form-ajout-livre.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { BuyDialogComponent } from './buy-dialog/buy-dialog.component';
 
 @NgModule({
   declarations: [
-    FormAjoutLivreComponent,
-    LibraryPresentationComponent
+    LibraryPresentationComponent,
+    BuyDialogComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    MatDialogModule,
     BookModule,
     DashboardModule
   ],
   exports:[
-    FormAjoutLivreComponent,
     LibraryPresentationComponent,
     BookModule
   ]
